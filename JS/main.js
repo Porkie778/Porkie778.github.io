4b130cc69e6d0a24076b7ba221edf89862ec8312
Benjamin <benjbruce778@gmail.com>
2023-03-08 21:23:35 +0000

    Refined media queries for mobile and desktop nav.html modified

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener('load', () => {
 	const loader = document.querySelector('.loader');
 	loader.classList.add('loader-hidden');
@@ -20,17 +22,3 @@ function closeNav() {
 	document.getElementById('mobile-nav').style.width = '0';
 	document.body.style.overflow = 'scroll';
 }
-
-function navDisplay() {
-	let desktop = document.getElementById('desktop-nav');
-	let mobile = document.getElementById('mobile-nav');
-	if(window.innerWidth > 500){
-		desktop.style.display = "flex";
-		mobile.style.display = "none";
-	}
-	else {
-		window.alert("Hello");
-	}
-}
-
-window.addEventListener("resize",navDisplay(),false);
