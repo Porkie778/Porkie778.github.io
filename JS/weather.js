6867adc6dd391210aea213c9f6be03a80ea92bea
Porkie778 <benjbruce778@gmail.com>
2023-03-09 14:00:09 +0000

    Javascript fix v3.0

--- JS/weather.js
+++ JS/weather.js
@@ -1,7 +1,7 @@
 // API_KEY = 3b9f01fac5768ab53ac2483503953b21
 // cityID =  2964574
 function getWeatherData(cityID) {
-	var key = '3b9f01fac5768ab53ac2483503953b21';
+	let key = '3b9f01fac5768ab53ac2483503953b21';
 	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
 		.then(function (resp) {
 			return resp.json();
@@ -15,38 +15,34 @@ function getWeatherData(cityID) {
 }
 
 function drawWeather(d) {
-	var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+	let celcius = Math.round(parseFloat(d.main.temp) - 273.15);
 	document.getElementById('desc').innerHTML = d.weather[0].description;
 	document.getElementById('temp').innerHTML = celcius + '&deg;C';
 	document.getElementById('location').innerHTML = d.name;
 }
 
-function cityWeather() {
-	var city = document.getElementById('city-select');
+function cityChoice() {
+	let city = document.getElementById('city-select');
 	const value = city.value;
 	switch (value) {
 		case 'dublin':
 			return 2964574;
-			break;
 		case 'london':
 			return 2643743;
-			break;
 		case 'sydney':
 			return 2147714;
-			break;
 		case 'new-york':
 			return 5128581;
-			break;
+
 		case 'tokyo':
 			return 1850147;
-			break;
 		default:
-			break;
+			return 2964574;
 	}
 }
 
 document.getElementById('city-select').addEventListener('change', function () {
-	getWeatherData(cityWeather());
+	getWeatherData(cityChoice());
 });
 getWeatherData(cityWeather());
 
