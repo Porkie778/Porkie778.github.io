d3ff983185d5cf91f9235f3ad30cde609546d346
Benjamin <benjbruce778@gmail.com>
2023-03-13 01:23:44 +0000

    Media Query clean-up Form 'Go Back' button added' Height for contact form formatted correctly Modified mobile navigation bar styling

--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-
-
 window.addEventListener('load', () => {
 	document.getElementsByClassName('loader')[0].classList.add('loader-hidden');
 	document.getElementsByClassName('loader')[0].addEventListener('transitionend', () => {
@@ -9,12 +7,16 @@ window.addEventListener('load', () => {
 
 // Open navigation menu sidebar
 function openNav() {
-	document.getElementById('mobile-nav').style.width = '250px';
+	document.getElementById('mobile-nav').style.width = '100vw';
+	document.getElementById('openBtn').style.display = 'none';
 	document.body.style.overflow = 'hidden';
+	document.getElementById('main').style.filter = 'blur(4px)';
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
 	document.getElementById('mobile-nav').style.width = '0';
 	document.body.style.overflow = 'scroll';
+	document.getElementById('openBtn').style.display = 'block';
+	document.getElementById('main').style.filter = 'unset';
 }
