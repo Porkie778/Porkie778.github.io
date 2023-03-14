0aecb437b7e14ee057b2e551fc0b8ce56cc95f1e
Benjamin <benjbruce778@gmail.com>
2023-03-14 20:52:03 +0000

    Test of SCSS

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import './common.scss';
-
-window.addEventListener('load', () => {
-	document.getElementsByClassName('loader')[0].classList.add('loader-hidden');
-	document.getElementsByClassName('loader')[0].addEventListener('transitionend', () => {
-		document.body.removeChild('loader');
-	});
-});
