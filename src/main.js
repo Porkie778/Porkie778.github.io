001f2bd9b543b30d9d034639c5bff20bce671021
Benjamin <benjbruce778@gmail.com>
2023-03-14 21:05:04 +0000

    Reduced javascript functions and added click listener

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
-import './common.scss';
-import '/src/nav.js';
-
 window.addEventListener('load', () => {
 	document.getElementsByClassName('loader')[0].classList.add('loader-hidden');
 	document.getElementsByClassName('loader')[0].addEventListener('transitionend', () => {
 		document.body.removeChild('loader');
 	});
+});
+
+// Open navigation menu sidebar
+function openNav() {
+	document.getElementById('mobile-nav').style.width = '100vw';
+	document.getElementById('openBtn').style.display = 'none';
+	document.body.style.overflow = 'hidden';
+	// document.getElementById('main').style.filter = 'blur(4px)';
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById('mobile-nav').style.width = '0';
+	document.body.style.overflow = 'scroll';
+	document.getElementById('openBtn').style.display = 'block';
+	// document.getElementById('main').style.filter = 'unset';
+}
+
+document.getElementById('openBtn').addEventListener('click', () => {
+	openNav();
+});
+
+document.getElementById('closeBtn').addEventListener('click', () => {
+	closeNav();
 });
